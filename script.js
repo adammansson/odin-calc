82a7fafdf7763260e37c66360e0d54a8a54b015f
Adam Månsson <adam.s.mansson@gmail.com>
2022-05-06 15:27:00 +0200

    Added functioning RPN calculator, working on history

--- script.js
+++ script.js
@@ -1,71 +1,84 @@
 "use strict";
-const add = (i, j) => i + j;
-const subtract = (i, j) => i - j;
-const multiply = (i, j) => i * j;
-const divide = (i, j) => i / j;
-const operate = (operator, i, j) => {
-    switch (operator) {
-        case "+":
-            return add(i, j);
-        case "-":
-            return subtract(i, j);
-        case "*":
-            return multiply(i, j);
-        case "/":
-            return divide(i, j);
+const OPERATORS = ["+", "-", "*", "/"];
+function add(i, j) { return i + j; }
+function subtract(i, j) { return i - j; }
+function multiply(i, j) { return i * j; }
+function divide(i, j) { return i / j; }
+function evalRPN(tokens) {
+    const stack = [];
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        console.log(token);
+        if (OPERATORS.includes(token)) {
+            const rightOperand = stack.pop();
+            const leftOperand = stack.pop();
+            switch (token) {
+                case "+":
+                    stack.push(add(leftOperand, rightOperand));
+                    break;
+                case "-":
+                    stack.push(subtract(leftOperand, rightOperand));
+                    break;
+                case "*":
+                    stack.push(multiply(leftOperand, rightOperand));
+                    break;
+                case "/":
+                    if (rightOperand === 0) {
+                        stack.push(0);
+                    }
+                    else {
+                        stack.push(divide(leftOperand, rightOperand));
+                    }
+                    break;
+            }
+        }
+        else {
+            stack.push(+token);
+        }
     }
-};
-const container = document.querySelector("#container");
-const display = document.createElement("div");
-display.textContent = "0";
-let storedValue = 0;
-let storedOperator = "";
-if (container) {
-    container.appendChild(display);
-    for (let i = 0; i < 10; i++) {
-        const node = document.createElement("button");
-        node.textContent = i.toString();
-        node.addEventListener("click", () => {
-            if (display.textContent === "0" || display.textContent === storedValue.toString()) {
-                display.textContent = i.toString();
-            }
-            else {
-                display.textContent += i.toString();
-            }
-        });
-        container.appendChild(node);
-    }
-    ;
-    ["+", "-", "*", "/"].forEach(op => {
-        const node = document.createElement("button");
-        node.textContent = op;
-        node.addEventListener("click", () => {
-            if (storedValue === 0) {
-                storedValue = +display.textContent;
-                storedOperator = op;
-            }
-            else {
-                storedValue = operate(storedOperator, storedValue, +display.textContent);
-                storedOperator = op;
-                display.textContent = storedValue.toString();
-            }
-        });
-        container.appendChild(node);
-    });
-    const equals = document.createElement("button");
-    equals.textContent = "=";
-    equals.addEventListener("click", () => {
-        storedValue = operate(storedOperator, storedValue, +display.textContent);
-        display.textContent = storedValue.toString();
-    });
-    container.appendChild(equals);
-    const clear = document.createElement("button");
-    clear.textContent = "C";
-    clear.addEventListener("click", () => {
-        storedValue = 0;
-        storedOperator = "";
-        display.textContent = "0";
-    });
-    container.appendChild(clear);
+    return stack[0];
 }
-;
+const container = document.querySelector("#button-container");
+const output = document.querySelector("#output");
+const input = document.querySelector("#input");
+["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "_"].forEach(c => {
+    const node = document.createElement("div");
+    node.classList.add("button");
+    node.textContent = c;
+    node.addEventListener("click", () => {
+        input.textContent += c;
+        output.textContent = evalRPN(input.textContent.split("_")).toString();
+    });
+    container.appendChild(node);
+});
+const clear = document.createElement("div");
+clear.classList.add("button");
+clear.textContent = "C";
+clear.addEventListener("click", () => {
+    output.textContent = "";
+    input.textContent = "";
+});
+container.appendChild(clear);
+const del = document.createElement("div");
+del.classList.add("button");
+del.textContent = "D";
+del.addEventListener("click", () => {
+    input.textContent = input.textContent.slice(0, input.textContent.length - 1);
+    output.textContent = evalRPN(input.textContent.split("_")).toString();
+});
+container.appendChild(del);
+const history_container = document.querySelector("#history-container");
+const save = document.createElement("div");
+save.classList.add("button");
+save.textContent = "S";
+save.addEventListener("click", () => {
+    const node = document.createElement("div");
+    node.classList.add("history-event");
+    node.textContent = output.textContent;
+    node.addEventListener("click", () => {
+        input.textContent = node.textContent;
+        output.textContent = evalRPN(input.textContent.split("_")).toString();
+    });
+    history_container.appendChild(node);
+});
+container.appendChild(save);
--- script.ts
+++ script.ts
@@ -1,76 +1,95 @@
-const add = (i: number, j: number) => i + j;
-const subtract = (i: number, j: number) => i - j;
-const multiply = (i: number, j: number) => i * j;
-const divide = (i: number, j: number) => i / j;
+const OPERATORS = ["+", "-", "*", "/"];
 
-const operate = (operator: string, i: number, j: number) => {
-    switch (operator) {
-        case "+":
-            return add(i, j);
+function add(i: number, j: number): number { return i + j }
+function subtract(i: number, j: number): number { return i - j }
+function multiply(i: number, j: number): number { return i * j }
+function divide(i: number, j: number): number { return i / j }
 
-        case "-":
-            return subtract(i, j);
+function evalRPN(tokens: string[]): number {
+    const stack = [];
 
-        case "*":
-            return multiply(i, j);
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        console.log(token);
 
-        case "/":
-            return divide(i, j);
+        if (OPERATORS.includes(token)) {
+            const rightOperand: number = stack.pop();
+            const leftOperand: number = stack.pop();
+
+            switch (token) {
+                case "+":
+                    stack.push(add(leftOperand, rightOperand));
+                    break;
+
+                case "-":
+                    stack.push(subtract(leftOperand, rightOperand));
+                    break;
+
+                case "*":
+                    stack.push(multiply(leftOperand, rightOperand));
+                    break;
+
+                case "/":
+                    if (rightOperand === 0) {
+                        stack.push(0);
+                    } else {
+                        stack.push(divide(leftOperand, rightOperand));
+                    }
+                    break;
+            }
+        } else {
+            stack.push(+token);
+        }
     }
-};
+    return stack[0];
+}
 
-const container = document.querySelector("#container");
-const display = document.createElement("div");
-display.textContent = "0";
-let storedValue = 0;
-let storedOperator = "";
+const container = document.querySelector("#button-container");
+const output = document.querySelector("#output");
+const input = document.querySelector("#input");
 
-if (container) {
-    container.appendChild(display)
-
-    for (let i = 0; i < 10; i++) {
-        const node = document.createElement("button");   
-        node.textContent = i.toString();
-        node.addEventListener("click", () => {
-            if (display.textContent === "0" || display.textContent === storedValue.toString()) {
-                display.textContent = i.toString()
-            } else {
-                display.textContent += i.toString()
-            }
-        });
-        container.appendChild(node);
-    };
-
-    ["+", "-", "*", "/"].forEach(op => {
-        const node = document.createElement("button");   
-        node.textContent = op;
-        node.addEventListener("click", () => {
-            if (storedValue === 0) {
-                storedValue = +display.textContent;
-                storedOperator = op;
-            } else {
-                storedValue = operate(storedOperator, storedValue, +display.textContent);
-                storedOperator = op;
-                display.textContent = storedValue.toString()
-            }
-        });
-        container.appendChild(node); 
+["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "_"].forEach(c => {
+    const node = document.createElement("div");
+    node.classList.add("button")
+    node.textContent = c;
+    node.addEventListener("click", () => {
+        input.textContent += c;
+        output.textContent = evalRPN(input.textContent.split("_")).toString();
     });
+    container.appendChild(node);
+});
 
-    const equals = document.createElement("button");
-    equals.textContent = "=";
-    equals.addEventListener("click", () => {
-        storedValue = operate(storedOperator, storedValue, +display.textContent);
-        display.textContent = storedValue.toString()
-    });
-    container.appendChild(equals);
+const clear = document.createElement("div");
+clear.classList.add("button")
+clear.textContent = "C";
+clear.addEventListener("click", () => {
+    output.textContent = "";
+    input.textContent = "";
+});
+container.appendChild(clear);
 
-    const clear = document.createElement("button");
-    clear.textContent = "C";
-    clear.addEventListener("click", () => {
-        storedValue = 0;
-        storedOperator = "";
-        display.textContent = "0";
+const del = document.createElement("div");
+del.classList.add("button");
+del.textContent = "D";
+del.addEventListener("click", () => {
+    input.textContent = input.textContent.slice(0, input.textContent.length - 1);
+    output.textContent = evalRPN(input.textContent.split("_")).toString();
+});
+container.appendChild(del);
+
+const history_container = document.querySelector("#history-container");
+
+const save = document.createElement("div");
+save.classList.add("button");
+save.textContent = "S";
+save.addEventListener("click", () => {
+    const node = document.createElement("div");
+    node.classList.add("history-event")
+    node.textContent = output.textContent;
+    node.addEventListener("click", () => {
+        input.textContent = node.textContent;
+        output.textContent = evalRPN(input.textContent.split("_")).toString();
     });
-    container.appendChild(clear);
-};
+    history_container.appendChild(node);
+});
+container.appendChild(save);
